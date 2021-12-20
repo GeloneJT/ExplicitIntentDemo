d5437434eb1f3240bcde6dc40acd304f7384143d
Jelani Thomas <jelani13@icloud.com>
2021-12-20 16:29:31 -0500

    MainActivity.kt code cleanup

--- app/src/main/java/com/example/explicitintentdemo/MainActivity.kt
+++ app/src/main/java/com/example/explicitintentdemo/MainActivity.kt
@@ -4,14 +4,13 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var btn1: Button
-    lateinit var btn2: Button
-    lateinit var btn3: Button
+    private lateinit var btn1: Button
+    private lateinit var btn2: Button
+    private lateinit var btn3: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,21 +20,21 @@ class MainActivity : AppCompatActivity() {
         btn2 = findViewById(R.id.b2)
         btn3 = findViewById(R.id.b3)
 
-        btn1.setOnClickListener(View.OnClickListener {
+        btn1.setOnClickListener{
             intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://amazon.com"))
             startActivity(intent)
-        })
-        btn2.setOnClickListener(View.OnClickListener {
+        }
+        btn2.setOnClickListener{
             intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:123-456-789"))
             startActivity(intent)
-        })
-        btn3.setOnClickListener(View.OnClickListener {
-            var location = Uri.parse("geo:27.2046,77.4977")
-            var mapIntent = Intent(Intent.ACTION_VIEW, location)
+        }
+        btn3.setOnClickListener{
+            val location = Uri.parse("geo:0,0?q=1600+Amphitheatre+Parkway,+Mountain+View,+California")
+            val mapIntent = Intent(Intent.ACTION_VIEW, location)
             mapIntent.setPackage("com.google.android.apps.maps")
             mapIntent.resolveActivity(packageManager)?.let {
                 startActivity(mapIntent)
             }
-        })
+        }
     }
 }
